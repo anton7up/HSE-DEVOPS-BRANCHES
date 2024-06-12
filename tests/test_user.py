37bef29c821a1a520b4807a55eb4b2f2f9972c20
anton7up <99725170+anton7up@users.noreply.github.com>
2024-06-12 10:50:57 +0300

    Update test_user.py

--- tests/test_user.py
+++ tests/test_user.py
@@ -7,6 +7,10 @@ def test_register_new_user():
     assert user1.name == "Ivan"
     assert user1.lastname == "Ivanov"
 
+def test_register_new_user_fail():
+    with pytest.raises(ValueError):
+         User("1234", "Ivan", "Ivanov", "123@gmail.com", "12345678")
+
 def test_get_username():
     user2 = User("1234", "Ivan", "Ivanov", "123@gmail.com", "2374bhwdh6G")
     assert user2.get_username() == "1234"
